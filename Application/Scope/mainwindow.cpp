e7d255b3e7a6025b5aae65d415dbb174ec90fc66
Jens Geudens <jensgeudens@hotmail.com>
2026-02-27 22:25:47 +0100

    Fix absolute path

--- Application/Scope/mainwindow.cpp
+++ Application/Scope/mainwindow.cpp
@@ -1,11 +1,40 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QCoreApplication>
+#include <QDir>
+#include <QFile>
+
+static QString findPluginLibraryPath()
+{
+    QDir dir(QCoreApplication::applicationDirPath());
+#if defined(Q_OS_WIN)
+    QString libName = "example_plugin.dll";
+#elif defined(Q_OS_MAC)
+    QString libName = "libexample_plugin.dylib";
+#else
+    QString libName = "libexample_plugin.so";
+#endif
+
+    QDir searchDir(dir);
+    for (int i = 0; i < 8; ++i)
+    {
+        QString pluginDir = searchDir.filePath("Plugins/example");
+        QString candidate = QDir(pluginDir).filePath(libName);
+        if (QFile::exists(candidate))
+            return QDir(pluginDir).absolutePath();
+        if (!searchDir.cdUp())
+            break;
+    }
+    return QDir(dir).filePath("Plugins/example");
+}
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), _ui(new Ui::MainWindow)
 {
     _ui->setupUi(this);
 
-    _pluginManager.loadPluginsFromDir("/home/jens/Projects/ProtocolPlugin/Application/build/Plugins/example");
+    // Load plugin directory determined at runtime
+    QString pluginPath = findPluginLibraryPath();
+    _pluginManager.loadPluginsFromDir(pluginPath);
 
     // Loop over plugin and print info to status bar for demo purposes
     for (PluginInterface::AbstractProtocolPlugin* plugin : _pluginManager.plugins())
